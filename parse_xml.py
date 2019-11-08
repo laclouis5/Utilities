5797a4b36c2c33220aece06813abcc34f9d761a3
Louis Lac <lac.louis5@gmail.com>
2019-11-08 11:51:39 +0100

    TODO: move Yolo parsers here

--- parse_xml.py
+++ parse_xml.py
@@ -46,28 +46,18 @@ def parse_xml(directories, classes=None):
 
     return allBoundingBoxes
 
-# if __name__ == '__main__':
-#
-# 	base_path = '/media/deepwater/Data2/Louis/RetinaNet/datasets/'
-# 	classes   = ['mais', 'haricot', 'carotte']
-#
-# 	folders = [
-# 		'training_set/mais_haricot_feverole_pois/50/1',
-# 		'training_set/mais_haricot_feverole_pois/50/2',
-# 		'training_set/mais_haricot_feverole_pois/60/1',
-# 		'training_set/mais_haricot_feverole_pois/60/2',
-# 		'training_set/mais_haricot_feverole_pois/100/1',
-# 		'training_set/mais_haricot_feverole_pois/100/2',
-# 	]
-#
-# 	validation_folder = [
-# 		"validation1/"
-# 		"validation2/"
-# 		"validation3/"
-# 	]
-#
-# 	directories = [os.path.join(base_path, folder) for folder in folders]
-#
-#     allBoundingBoxes = parse_xml(directories, classes)
-#     print('count: {}'.format(allBoundingBoxes.count()))
-#     print(allBoundingBoxes.stats())
+
+def parse_yolo_folder(directories, classes=None):
+    # Put here the code to parse Yolo subfolders and create a Parser Class
+    # and create a Python Package
+    return
+
+def parse_yolo_dir(directories, classes=None):
+    # Put here the code to parse Yolo subfolders and create a Parser Class
+    # and create a Python Package
+    return
+
+def parse_yolo_file(directories, classes=None):
+    # Put here the code to parse Yolo subfolders and create a Parser Class
+    # and create a Python Package
+    return
